f4cf4fe64646dae56790edc7aad02c8991521dc2
Marco 10 <Marco 10@DESKTOP-IA3DLKN>
2019-06-17 00:22:28 -0300

    Primeiro commit

--- src/codersfootballleague2/pkg0/negocios/beans/PartidaOficial.java
+++ src/codersfootballleague2/pkg0/negocios/beans/PartidaOficial.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package codersfootballleague2.pkg0.negocios.beans;
 
-/**
- *
- * @author Marco 10
- */
+
 public class PartidaOficial {
     
     private int qtdPagantes;
